6579769de58afc5e74bfca5bf7cbc4bd7f965419
Georgia <gimarg21@gmail.com>
2025-05-15 16:35:14 +0300

    render updates

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const boardRoutes = require("./routes/boardRoutes");
 const voteRoutes = require("./routes/voteRoutes");
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
@@ -21,8 +22,6 @@ mongoose
   })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(process.env.PORT, () =>
-      console.log(`Server running on port ${process.env.PORT}`)
-    );
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((err) => console.error("MongoDB connection failed:", err));
